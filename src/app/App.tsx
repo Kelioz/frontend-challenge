3c7eb36a91ab006609f03f85fff3cc19e0660e21
Maxim <maxkryu2004@gmail.com>
2026-04-07 12:02:10 +0300

    git hub pages fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,14 @@
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from '../shared/config/react-query'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Router } from './router'
 import './styles.scss'
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <HashRouter>
         <Router />
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   )
 }
